df469dee2d2ceabab0c82455b29f39244f19e1dc
hwanjuson <hwanjuson@gmail.com>
2021-10-21 15:16:08 +0900

    장애물 랜덤 속도 조정, 스테이지 모드 설정하는중 등 그 외 여러가지

--- Gogeumdo/Assets/Scripts/Manager/StageManager.cs
+++ Gogeumdo/Assets/Scripts/Manager/StageManager.cs
@@ -13,10 +13,10 @@ public class StageManager : MonoBehaviour
     {
         stageButton = btnParent.GetComponentsInChildren<Button>(); //버튼들 가져오기
 
-        for (int i = 0; i < stageButton.Length; i++)
+        for (int i = 1; i < stageButton.Length + 1; i++)
         {
             int idx = i; //클로저 문제로 인하여 이렇게 써야한다.
-            stageButton[idx].onClick.AddListener(() =>
+            stageButton[idx - 1].onClick.AddListener(() =>
             {
                 GameManager.instance.stageIndex = idx; //게임매니저의 스테이지 인덱스를 바꿔줌
                 LoadManager.LoadScene("InGame");
